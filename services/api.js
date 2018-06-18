3cdc0b4bc3d4eb28d865b80cb483239f045ac29c
Venkata Nagendra Rao Saragadam <svnagendra@gmail.com>
2018-06-18 18:20:08 +0530

    Change

--- services/api.js
+++ services/api.js
@@ -1,5 +1,5 @@
 exports.name=function(req, res) {
-    res.json({
-      "Output": "Hello World! My Name is Test."
+    res.send({
+      "Output": "Hello World! My Name is Test.. You called Myguru-Api."
     });
 };
